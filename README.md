0b4a9d334a8f42064ec933d3676b966b31a0f924
polinamoore <p_gerasimenko@yahoo.com>
2021-08-13 17:27:20 +0100

    Add initial client folders

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# client
+# Quizz App
---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Repo } from './pages';
+import { Header } from './layout';
+import "./style.css";
+
+function App(){
+
+  return(
+    <>
+    <Header />
+    <Repo/>
+    </>
+  );
+
+};
+
+export default App;
