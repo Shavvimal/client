a2c36ca02a58aee59c9329081dc3ea62603a3dd3
Shav <shavhugan@gmail.com>
2021-08-16 13:27:34 +0100

    added redux provider and router

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import App from './App.js';
+import store from './store';
+
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
