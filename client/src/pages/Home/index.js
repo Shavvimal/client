ff3d6c794c145b356d734da2e87f5ed7962a1258
Shav <shavhugan@gmail.com>
2021-08-16 16:47:36 +0100

    made API call and assigned required values to card props, before rendering cards

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,45 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './styleHome.css'
 import { NavLink } from 'react-router-dom';
+import { Card } from '../../components'
+import axios from 'axios';
 
 const Home = () => {
+
+    const [qnsState, setQns] = useState([]);
+
+    useEffect(() => fetchQns(), [])
+
+
+    const fetchQns = async () => {
+        try {
+            let qns = await axios.get(`https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple`);
+            console.log(qns);
+            const array = qns.data.results.map(qns => {
+                let question = qns.question;
+                let correct_answer = qns.correct_answer;
+                let incorrect_answers = qns.incorrect_answers;
+
+                return { question, incorrect_answers, correct_answer }
+            })
+            setQns(array)
+        }
+        catch (err) {
+            console.warn(err);
+        }
+    }
+
+
+    const renderCards = data => data.map((qn, i) => <Card question={qn.question} key={i} incorrect_answers={qn.incorrect_answers} correct_answer={qn.correct_answer} />)
+
     return (
-        <p>Home sweet home</p>
+
+        <>
+
+            {renderCards(qnsState)};
+        </>
+
+
     )
 }
 
