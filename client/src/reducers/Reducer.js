9e65bec43661601f75e98fa0fe15c2f2cc4be5ff
polinamoore <p_gerasimenko@yahoo.com>
2021-08-16 23:29:58 +0100

    Add initial Reducer code

--- client/src/reducers/Reducer.js
+++ client/src/reducers/Reducer.js
@@ -1,12 +1,13 @@
-const initState = {
-}
+const initState = { username: "", category: "", difficulty: "", result: {quiz: []}, loading: false };
 
 const Reducer = (state=initState, action) => {
     switch(action.type) {
-        case '':
-            return ({
-                ...state,
-             })
+        case 'LOADING':
+            return { ...state, result: action.payload, loading: true };
+        case 'LOAD_QUIZ':
+            return { ...state, result: action.payload, loading: false, error: false};
+        case 'SET ERROR':
+            return { ...state, error: action.payload, loading: false};
         default:
             return state;
     }
