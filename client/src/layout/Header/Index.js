202c2df13c0a43cb2f1048127f039fa521fef015
Shav <shavhugan@gmail.com>
2021-08-16 12:21:41 +0100

    added header with humzas name

--- client/src/layout/Header/Index.js
+++ client/src/layout/Header/Index.js
@@ -1,1 +1,18 @@
 // Import and use Leaderboard here
+import React from 'react';
+import './style.css';
+
+
+
+const Header = () => {
+
+    return (
+
+
+        <p> HumzaqAassasdasdasdaa</p>
+
+    );
+
+}
+
+export default Header;
