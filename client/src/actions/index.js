1386b13fece33234c0535ace7a45b5098eea77c5
polinamoore <p_gerasimenko@yahoo.com>
2021-08-17 00:00:23 +0100

    Update actions.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,31 +2,41 @@ import axios from 'axios';
 
 const loading = userEntries => ({ type: 'LOADING', payload: userEntries }); // Can payload be an object??
 
-const [ quiz, setQuiz] = useState([]);
-    const [ error, setError ] = useState("");
+const loadResult = ({ results: { quiz } }) => ({ 
+    type: 'LOAD_RESULT',
+    payload: { quiz }  // Need to destructure?
+});
 
-    const getQuiz = async (userChoices) => {
-        try{
-            setError(null);
-            let { data }  = await axios.get(`https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`);
-
-            if (!data.results.length){
-                setError("Sorry, no quizzes available!")
-            }
-            else {
-                let quizArray = data.results.map(el => {
-
-                    let questionTemp = el.question.replaceAll('&quot;', "'");
-                    let question = questionTemp.replaceAll('&#039;', "'");
-                    let correctAnswer = el.correct_answer;
-                    let incorrectAnswers = el.incorrect_answers.map(el => el.replaceAll('&#039;', "'")); // an array
-                    return {question, correctAnswer, incorrectAnswers};
-                    });
-                    setQuiz(quizArray);
-                    console.log(quizArray);
-            }
-        } catch(err) {
+// Need two dispatch functions, one each for category and difficulty??
+const getResult = ({ category, difficulty }) => {
+    return async dispatch => {
+        dispatch(loading(category, difficulty)); // Or need two dispatches?
+        try {
+            const quiz = await getQuiz(category, difficulty)
+        } catch (err) {
             console.warn(err.message);
-            setError("Sorry, no quizzes available!");
+            dispatch({ type: 'SET_ERROR', payload: err.message });
         }
     }
+}
+
+
+// Helpers
+const getQuiz = async userEntries => {
+    try{
+        const { data }  = await axios.get(`https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userEntries.difficulty}&type=multiple`);
+
+        const quizArray = data.results.map(el => {
+            const questionTemp = el.question.replaceAll('&quot;', "'");
+            const question = questionTemp.replaceAll('&#039;', "'");
+            const correctAnswer = el.correct_answer;
+            const incorrectAnswers = el.incorrect_answers.map(el => el.replaceAll('&#039;', "'"));
+            return {question, correctAnswer, incorrectAnswers}});
+        return quizArray;
+    } catch(err) {
+        if (data.status === 404) {
+            throw Error ('Quiz not available, sorry')
+        }
+        throw new Error (err.message);
+    }
+}
