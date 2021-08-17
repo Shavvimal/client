6bf70249c65989128d562f9c9e51967cc4d9670e
Shav <shavhugan@gmail.com>
2021-08-17 11:29:44 +0100

    reducer renamed to match Humza

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import { questionsReducer } from './reducers'
-
-const store = createStore(questionsReducer, composeWithDevTools(applyMiddleware(thunk)))
+import questionReducer from "./reducers/questionReducer";
+const store = createStore(questionReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
