8f7af472261ffb9771cd49c07ac43799b63ef964
Shav <shavhugan@gmail.com>
2021-08-17 10:44:42 +0100

    added replaceAmp function to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,29 +1,38 @@
 import axios from 'axios';
 
-const [ quiz, setQuiz] = useState([]);
-    const [ error, setError ] = useState("");
+const [quiz, setQuiz] = useState([]);
+const [error, setError] = useState("");
 
-    const getQuiz = async (userChoices) => {
-        try{
+let replaceAmp = (str) => {
+    str = str.replaceAll("&quot;", "\"");
+    str = str.replaceAll("&amp; ", " & ");
+    str = str.replaceAll("&#039;", "'");
+    return str
+}
+
+
+export const getQuiz =
+
+    async (userChoices) => {
+        try {
             setError(null);
-            let { data }  = await axios.get(`https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`);
+            let { data } = await axios.get(`https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`);
 
-            if (!data.results.length){
+            if (!data.results.length) {
                 setError("Sorry, no quizzes available!")
             }
             else {
                 let quizArray = data.results.map(el => {
 
-                    let questionTemp = el.question.replaceAll('&quot;', "'");
-                    let question = questionTemp.replaceAll('&#039;', "'");
+                    let question = replaceAmp(el.question);
                     let correctAnswer = el.correct_answer;
-                    let incorrectAnswers = el.incorrect_answers.map(el => el.replaceAll('&#039;', "'")); // an array
-                    return {question, correctAnswer, incorrectAnswers};
-                    });
-                    setQuiz(quizArray);
-                    console.log(quizArray);
+                    let incorrectAnswers = el.incorrect_answers.map(el => replaceAmp(el)); // an array
+                    return { question, correctAnswer, incorrectAnswers };
+                });
+                setQuiz(quizArray);
+                console.log(quizArray);
             }
-        } catch(err) {
+        } catch (err) {
             console.warn(err.message);
             setError("Sorry, no quizzes available!");
         }
