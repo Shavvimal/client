66f35656a74eed2e8e87854a04c686afe5588536
Shav <shavhugan@gmail.com>
2021-08-17 15:48:57 +0100

    added cases in reducer

--- client/src/reducers/questionReducer.js
+++ client/src/reducers/questionReducer.js
@@ -1,19 +1,40 @@
+
 const initState = {
   loading: false,
+  questionIndex: 0,
+  username: "",
   category: "",
   difficulty: "",
   result: [{ question: "", correctAnswer: "", incorrectAnswers: [] }],
+  score: 0,
+
 };
 
 const questionReducer = (state = initState, action) => {
   switch (action.type) {
     case "LOAD_QUIZ":
-      return { ...state, results: action.payload, error: false };
+      return { ...state, result: action.payload, error: false };
+    case "ADD_USERNAME":
+      return { ...state, username: action.payload, error: false };
+    case "ANSWER_SUBMIT":
+      if (action.payload === state.result[state.questionIndex].correctAnswer) {
+        return { ...state, score: state.score + 1, questionIndex: state.questionIndex + 1 };
+      }
+      return { ...state, questionIndex: state.questionIndex + 1 };
+
     case "SET_ERROR":
       return { ...state, error: action.payload };
+
     default:
       return state;
   }
 };
+
+// type: "LOAD_QUIZ",
+// payload: [{question: "What is my name?", correctAnswer:"Humza", incorrectAnswers:["Shav", "Polina", "Sammie"]}]
+
+// type: "ANSWER_SUBMIT",
+// payload: "Humza"
+
 
 export default questionReducer;
