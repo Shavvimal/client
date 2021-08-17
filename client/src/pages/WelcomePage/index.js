1444375fc08590fa63f1161b9493357faecbdaa8
humza1997 <humza786@gmail.com>
2021-08-17 14:23:12 +0100

    uncommented getQuiz

--- client/src/pages/WelcomePage/index.js
+++ client/src/pages/WelcomePage/index.js
@@ -1,41 +1,43 @@
 import React, { useState } from "react";
 import { UserForm } from "../../components";
+import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
+import { loadQuiz, addUsername } from "../../actions";
 
 function WelcomePage() {
   const [quiz, setQuiz] = useState([]);
   const [error, setError] = useState("");
 
-  // const getQuiz = async (userChoices) => {
-  //   try {
-  //     setError(null);
-  //     let { data } = await axios.get(
-  //       `https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`
-  //     );
+  const getQuiz = async (userChoices) => {
+    try {
+      setError(null);
+      let { data } = await axios.get(
+        `https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`
+      );
 
-  //     if (!data.results.length) {
-  //       setError("Sorry, no quizzes available!");
-  //     } else {
-  //       let quizArray = data.results.map((el) => {
-  //         let questionTemp = el.question.replaceAll("&quot;", "'");
-  //         let question = questionTemp.replaceAll("&#039;", "'");
-  //         let correctAnswer = el.correct_answer;
-  //         let incorrectAnswers = el.incorrect_answers.map((el) => el.replaceAll("&#039;", "'")); // an array
-  //         return { question, correctAnswer, incorrectAnswers };
-  //       });
-  //       setQuiz(quizArray);
-  //       console.log(quizArray);
-  //     }
-  //   } catch (err) {
-  //     console.warn(err.message);
-  //     setError("Sorry, no quizzes available!");
-  //   }
-  // };
+      if (!data.results.length) {
+        setError("Sorry, no quizzes available!");
+      } else {
+        let quizArray = data.results.map((el) => {
+          let questionTemp = el.question.replaceAll("&quot;", "'");
+          let question = questionTemp.replaceAll("&#039;", "'");
+          let correctAnswer = el.correct_answer;
+          let incorrectAnswers = el.incorrect_answers.map((el) => el.replaceAll("&#039;", "'")); // an array
+          return { question, correctAnswer, incorrectAnswers };
+        });
+        setQuiz(quizArray);
+        console.log(quizArray);
+      }
+    } catch (err) {
+      console.warn(err.message);
+      setError("Sorry, no quizzes available!");
+    }
+  };
 
   return (
     <>
       <UserForm getQuiz={getQuiz} />
-      {/* {quiz.map((el, index) => {
+      {quiz.map((el, index) => {
         return (
           <div key={index}>
             <p>{el.question}</p>
@@ -47,7 +49,7 @@ function WelcomePage() {
             <br></br>
           </div>
         );
-      })} */}
+      })}
     </>
   );
 }
