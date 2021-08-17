ded61b202c112e0b6dbc671f2807dbb80ce79f9e
Sammie <Lucidareverie@outlook.com>
2021-08-17 18:42:10 +0100

    tidy QuestionPage index.js of redundant code

--- client/src/pages/QuestionPage/index.js
+++ client/src/pages/QuestionPage/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { loadQuiz } from "../../actions";
 import { useSelector } from 'react-redux'; // New imports to work with Redux
 import './styleHome.css'
 import { Card } from '../../components'
@@ -7,22 +6,14 @@ import { Card } from '../../components'
 const QuestionPage = () => {
 
     const results = useSelector(state => state.result);
-    
     console.log(results)
     
-
-    const handleClick = () =>{
-        console.log("click!")
-        renderCards(results)
-    }
-
     const renderCards = data => data.map((qn, i) => <Card question={qn.question} key={i} incorrectAnswers={qn.incorrectAnswers} correctAnswer={qn.correctAnswer} />)
 
     return (
 
         <>
-            <button onClick={handleClick}>play game </button>
-            {/* {renderCards()}; */}
+            {renderCards(results)};
         </>
 
 
