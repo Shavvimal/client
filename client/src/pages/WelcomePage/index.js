c8d49197599d1f60c695b372941dc8bfd16be131
humza1997 <humza786@gmail.com>
2021-08-17 11:18:42 +0100

    commented out getQuiz function in welcome page

--- client/src/pages/WelcomePage/index.js
+++ client/src/pages/WelcomePage/index.js
@@ -6,31 +6,31 @@ function WelcomePage() {
   const [quiz, setQuiz] = useState([]);
   const [error, setError] = useState("");
 
-  const getQuiz = async (userChoices) => {
-    try {
-      setError(null);
-      let { data } = await axios.get(
-        `https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`
-      );
+  // const getQuiz = async (userChoices) => {
+  //   try {
+  //     setError(null);
+  //     let { data } = await axios.get(
+  //       `https://opentdb.com/api.php?amount=10&category=${userChoices.category}&difficulty=${userChoices.difficulty}&type=multiple`
+  //     );
 
-      if (!data.results.length) {
-        setError("Sorry, no quizzes available!");
-      } else {
-        let quizArray = data.results.map((el) => {
-          let questionTemp = el.question.replaceAll("&quot;", "'");
-          let question = questionTemp.replaceAll("&#039;", "'");
-          let correctAnswer = el.correct_answer;
-          let incorrectAnswers = el.incorrect_answers.map((el) => el.replaceAll("&#039;", "'")); // an array
-          return { question, correctAnswer, incorrectAnswers };
-        });
-        setQuiz(quizArray);
-        console.log(quizArray);
-      }
-    } catch (err) {
-      console.warn(err.message);
-      setError("Sorry, no quizzes available!");
-    }
-  };
+  //     if (!data.results.length) {
+  //       setError("Sorry, no quizzes available!");
+  //     } else {
+  //       let quizArray = data.results.map((el) => {
+  //         let questionTemp = el.question.replaceAll("&quot;", "'");
+  //         let question = questionTemp.replaceAll("&#039;", "'");
+  //         let correctAnswer = el.correct_answer;
+  //         let incorrectAnswers = el.incorrect_answers.map((el) => el.replaceAll("&#039;", "'")); // an array
+  //         return { question, correctAnswer, incorrectAnswers };
+  //       });
+  //       setQuiz(quizArray);
+  //       console.log(quizArray);
+  //     }
+  //   } catch (err) {
+  //     console.warn(err.message);
+  //     setError("Sorry, no quizzes available!");
+  //   }
+  // };
 
   return (
     <>
