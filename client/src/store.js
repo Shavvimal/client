da06ab083a71d1b5500c35c3b782d763db4594b6
Shav <shavhugan@gmail.com>
2021-08-17 10:42:07 +0100

    renamed reducer in store

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import Reducer from './reducers/Reducer'
-const store = createStore(Reducer, composeWithDevTools(applyMiddleware(thunk)))
+import { questionsReducer } from './reducers'
+
+const store = createStore(questionsReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
