835c1b7ccef03aac3740da03859fe23e1d74e5cf
humza1997 <humza786@gmail.com>
2021-08-17 12:10:33 +0100

    added add username case and answersubmit case to the reducer

--- client/src/reducers/questionReducer.js
+++ client/src/reducers/questionReducer.js
@@ -1,16 +1,28 @@
 const initState = {
   loading: false,
+  questionIndex: 0,
+  username: "",
   category: "",
   difficulty: "",
   result: [{ question: "", correctAnswer: "", incorrectAnswers: [] }],
+  score: 0,
 };
 
 const questionReducer = (state = initState, action) => {
   switch (action.type) {
     case "LOAD_QUIZ":
-      return { ...state, results: action.payload, error: false };
+      return { ...state, result: action.payload, error: false };
+    case "ADD_USERNAME":
+      return { ...state, username: action.payload, error: false };
+    case "ANSWER_SUBMIT":
+      if (action.payload === state.result[questionIndex].correctAnswer) {
+        return { ...state, score: state.score++, questionIndex: state.questionIndex++ };
+      }
+      return { ...state, questionIndex: state.questionIndex++ };
+
     case "SET_ERROR":
       return { ...state, error: action.payload };
+
     default:
       return state;
   }
