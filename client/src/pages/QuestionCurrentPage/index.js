b0967b60d66e1a07f0774782a7355238238d5ea4
polinamoore <p_gerasimenko@yahoo.com>
2021-08-18 13:17:15 +0100

    Add submitData function to the last questions page

--- client/src/pages/QuestionCurrentPage/index.js
+++ client/src/pages/QuestionCurrentPage/index.js
@@ -5,13 +5,31 @@ import { Card } from "../../components";
 import { scrubStr, shuffle, resetState } from "../../actions";
 import { Answer } from "../../components";
 import { useHistory } from "react-router";
+import axios from 'axios';
 
 const QuestionCurrentPage = () => {
+  const username = useSelector((state) => state.username);
+  const difficulty = useSelector((state) => state.difficulty);
   const currentScore = useSelector((state) => state.score);
   const currentQuestionIndex = useSelector((state) => state.questionIndex);
   let results = useSelector((state) => state.result);
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const submitData = () => {
+
+    console.log('Submit Data is calling');
+
+    const req = {
+        name: username,
+        score: currentScore,
+        difficulty: difficulty
+      }
+    
+    axios.post('http://localhost:8080/leaderboard', req).then(response => {
+        console.log(response);
+      }).catch(console.warn);
+}
 
   function goHome() {
     dispatch(resetState());
@@ -34,8 +52,8 @@ const QuestionCurrentPage = () => {
         <br />
         <p className='font-semibold'> {scrubStr(results[currentQuestionIndex].question)} </p>
         <br />
-        {/* <p>Correct: {results[currentQuestionIndex].correctAnswer} </p>
-        <p>Incorrect: {results[currentQuestionIndex].incorrectAnswers} </p> */}
+        <p>Correct: {results[currentQuestionIndex].correctAnswer} </p>
+        {/* <p>Incorrect: {results[currentQuestionIndex].incorrectAnswers} </p> */}
         <p>Answers: {answers}</p>
         <p>
           Answer Buttons:{" "}
@@ -47,8 +65,11 @@ const QuestionCurrentPage = () => {
       </div>
     );
   } else {
+    
     console.log(currentQuestionIndex);
     return (
+      <>
+      {submitData()}
       <div className='border rounded-xl bg-white  mt-20 w-11/12 h-5/6 m-auto px-10 py-5 shadow-xl flex flex-col justify-center text-center'>
         <h1 className=''>You're finished now, go Home </h1>
         <h3 className=' '>Final Score: {currentScore} /10 </h3>
@@ -59,6 +80,7 @@ const QuestionCurrentPage = () => {
         </button>
         <button className='border mx-auto px-4 py-1 rounded-full bg-purple-500 text-white'>Leaderboard </button>
       </div>
+      </>
     );
   }
 };
