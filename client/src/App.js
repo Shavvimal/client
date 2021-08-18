ec5bc192bf3a5dfe5949e70cd0332ce3eed4bd54
Shav <shavhugan@gmail.com>
2021-08-18 16:31:13 +0100

    replaced app with just socket.io client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from 'react-router-dom';
 import { Header } from "./layout";
 import { QuestionPage, WelcomePage, QuestionCurrentPage } from "./pages";
 import "./styles/app.css";
+import { socket } from './socket'
+import io from "socket.io-client";
+const serverEndpoint = "http://127.0.0.1:5001";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Switch >
-        <Route exact path="/">
-          <WelcomePage />
-        </Route>
-        <Route exact path="/QuestionPage">
-          <QuestionCurrentPage />
-          {/* <QuestionPage /> */}
-        </Route>
-      </Switch>
-    </>
-  );
+class App extends Component {
+  state = { socket: null };
+
+  componentDidMount() {
+    const socket = io(deployedServer);
+    this.setState({ socket });
+  };
+
+  componentWillUnmount() {
+    this.state.socket.disconnect();
+  };
+
+  render() {
+    return (
+      <div id="App">Hi</div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <>
+
+
+//       <Header />
+//       <Switch >
+//         <Route exact path="/">
+//           <WelcomePage />
+//         </Route>
+//         <Route exact path="/QuestionPage">
+//           <QuestionCurrentPage />
+//           {/* <QuestionPage /> */}
+//         </Route>
+//       </Switch>
+//     </>
+//   );
+// }
 
 export default App;
