ff93ab0b775f7a94fb61da3dc407d1455c8e022d
Shav <shavhugan@gmail.com>
2021-08-18 17:23:55 +0100

    bradcasting emit messages on people joining the lobby

--- Socket/socket.js
+++ Socket/socket.js
@@ -9,8 +9,24 @@ const io = require("socket.io")(server, { cors: { origin: "*" } }) // integrate 
 io.on('connection', socket => {
     console.log("'Ello, who's this we got here?") // runs when client first connects
 
+    // get total number of client connections
+    const participantCount = io.engine.clientsCount
+
+    // send event only to new connecting client
+    socket.emit('admin-message', 'Hi there, new friend!')
+
+
+    // send event to all other clients (not new connecting client)
+    socket.broadcast.emit('admin-message', `A new friend has arrived!`)
+
+    // send event to all clients
+    io.emit('admin-message', `There is ${participantCount} x friend here now!`)
+
+
     socket.on("disconnect", socket => { // runs when client disconnects
         console.log("K bye then");
+
+
     });
 });
 
