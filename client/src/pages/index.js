d22bf9556953112a054a9329e28000116cbaf38e
Shav <shavhugan@gmail.com>
2021-08-18 19:54:42 +0100

    exporte the leaderboard in index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as QuestionPage } from "./QuestionPage";
+
 export { default as WelcomePage } from "./WelcomePage";
 export { default as QuestionCurrentPage } from "./QuestionCurrentPage";
 export { default as Lobby } from "./Lobby";
