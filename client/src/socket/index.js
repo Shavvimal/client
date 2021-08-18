462752d48cd2b0cb58fb8e2b5b73fc8bd7bee920
Shav <shavhugan@gmail.com>
2021-08-18 17:22:09 +0100

    added socket id to client side log and also handles admit message from socket server

--- client/src/socket/index.js
+++ client/src/socket/index.js
@@ -3,7 +3,9 @@ import io from "socket.io-client";
 const socket = io("http://localhost:5001")
 
 socket.on("connect", () => {
-    console.log("connected!")
+    console.log(`Socket.io has connected to ${socket.id}!`)
 });
+
+socket.on('admin-message', msg => console.log(msg));
 
 export { socket }
