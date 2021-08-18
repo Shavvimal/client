7a57b4f78ef3eec6f796a9f0c3af2ab9e0e1b13c
Shav <shavhugan@gmail.com>
2021-08-18 23:42:15 +0100

    changed the leaderboard entry from a dic into a table row, and added logic to change colour of the difficult entry based on difficulty

--- client/src/components/LeaderBoardEntry/index.js
+++ client/src/components/LeaderBoardEntry/index.js
@@ -2,17 +2,27 @@ import React from 'react';
 import './style.css'
 
 
-const LeaderBoardEntry = ({ name, score, difficulty }) => {
+const LeaderBoardEntry = ({ name, score, difficulty, place }) => {
 
 
 
     return (
-        <div className="flex flex-row">
-            <p> Name: {name}
-                Score: {score}
-                difficulty: {difficulty}
-            </p>
-        </div >
+
+        <tr class=" text-center">
+            <td>
+                {place}
+            </td>
+            <td>
+                {name}
+            </td>
+            <td >
+                {score}
+            </td>
+            <td class="p-3 font-bold">
+                <span className={`text-gray-50 rounded-full px-3 py-1 ${difficulty === "hard" ? "bg-green-400" : "bg-red-400"} ${difficulty === "medium" ? "bg-yellow-400" : "bg-red-400"}`}>{difficulty}</span>
+            </td>
+        </tr>
+
 
     );
 }
