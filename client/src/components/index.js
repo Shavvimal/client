50ce4df3fe78e6116024a9206c5cefc8cd7943df
Sammie <Lucidareverie@outlook.com>
2021-08-18 17:41:17 +0100

    exported countdown component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,5 @@ export { default as Card } from './Card';
 export { default as Word } from './Word';
 export { default as Answer } from './Answer';
 export { default as UserForm } from "./UserForm";
+export { default as Countdown } from "./Countdown";
 
