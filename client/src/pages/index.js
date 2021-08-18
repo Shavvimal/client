5f646956f4f4b88c689dd6c368283a805d1c2855
humza1997 <humza786@gmail.com>
2021-08-18 17:45:00 +0100

    exporting leaderboard page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,4 @@
-export { default as QuestionPage } from './QuestionPage';
+export { default as QuestionPage } from "./QuestionPage";
 export { default as WelcomePage } from "./WelcomePage";
 export { default as QuestionCurrentPage } from "./QuestionCurrentPage";
+export { default as Leaderboard } from "./LeaderboardPage";
