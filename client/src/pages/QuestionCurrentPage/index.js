0a75cff47b96a657d0b65a95cf01b87aaab69d78
polinamoore <p_gerasimenko@yahoo.com>
2021-08-18 09:57:32 +0100

    Clean up QuestionCurrectPage

--- client/src/pages/QuestionCurrentPage/index.js
+++ client/src/pages/QuestionCurrentPage/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux'; // New imports to work with Redux
 import './styleHome.css'
-import { Card } from '../../components'
 import { scrubStr, shuffle } from "../../actions"
 import { Answer } from '../../components';
 
@@ -12,7 +11,7 @@ const QuestionCurrentPage = () => {
     const currentScore = useSelector((state) => state.score);
     const currentQuestionIndex = useSelector((state) => state.questionIndex);
     let results = useSelector(state => state.result);
-    let results2 = results.concat({ question: "What is my name?", correctAnswer: "Humza", incorrectAnswers: ["Shav", "Polina", "Sammie"] })
+    // let results2 = results.concat({ question: "What is my name?", correctAnswer: "Humza", incorrectAnswers: ["Shav", "Polina", "Sammie"] })
 
     const answers = shuffle([...results[currentQuestionIndex].incorrectAnswers, results[currentQuestionIndex].correctAnswer]);
 
@@ -21,16 +20,16 @@ const QuestionCurrentPage = () => {
         return shuffledArr.map((t, i) => <Answer key={i} word={scrubStr(t)} />);
     }
 
-    if (currentQuestionIndex < 5) {
+    if (currentQuestionIndex < 9) {
         return (
-            <div class="border rounded-xl bg-white w-11/12 h-5/6 m-auto mt-20 px-10 py-5 shadow-xl">
-                <div class="flex flex-row justify-between ">
-                    <h1 class="">Question {currentQuestionIndex + 1} </h1>
-                    <h3 class=" ">Score {currentScore} </h3>
+            <div classNameName="border rounded-xl bg-white w-11/12 h-5/6 m-auto mt-20 px-10 py-5 shadow-xl">
+                <div classNameName="flex flex-row justify-between ">
+                    <h1 classNameName="">Question {currentQuestionIndex + 1} </h1>
+                    <h3 classNameName=" ">Score {currentScore} </h3>
                 </div>
 
                 <br />
-                <p class="font-semibold"> {scrubStr(results[currentQuestionIndex].question)} </p>
+                <p className="font-semibold"> {scrubStr(results[currentQuestionIndex].question)} </p>
                 <br />
                 <p>Correct: {results[currentQuestionIndex].correctAnswer}  </p>
                 <p>Incorrect: {results[currentQuestionIndex].incorrectAnswers}  </p>
@@ -41,10 +40,10 @@ const QuestionCurrentPage = () => {
         )
     } else {
         return (
-            <div class="border rounded-xl bg-white  mt-20 w-11/12 h-5/6 m-auto px-10 py-5 shadow-xl">
-                <div class="flex flex-row justify-between ">
-                    <h1 class="">You're finished now, go Home </h1>
-                    <h3 class=" ">Final Score: {currentScore} /10 </h3>
+            <div className="border rounded-xl bg-white  mt-20 w-11/12 h-5/6 m-auto px-10 py-5 shadow-xl">
+                <div className="flex flex-row justify-between ">
+                    <h1 className="">You're finished now, go Home </h1>
+                    <h3 className=" ">Final Score: {currentScore} /10 </h3>
                 </div>
             </div>
         )
