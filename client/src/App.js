12e4018ca44387248f53533b8ea6456ac6e38d30
Sammie <Lucidareverie@outlook.com>
2021-08-19 17:39:29 +0100

    Updated header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
-import { WelcomePage, QuestionCurrentPage, Leaderboard,  Header} from "./pages";
+import { WelcomePage, QuestionCurrentPage, Leaderboard, Header} from "./pages";
 import "./styles/app.css";
 
 function App() {
