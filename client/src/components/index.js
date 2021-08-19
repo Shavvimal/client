4c345230a68b2b154f7acd69bd07fb1d5123aa2a
humza1997 <humza786@gmail.com>
2021-08-19 23:23:43 +0100

    removed unnecessary components being exported

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,3 @@
-export { default as Card } from "./Card";
-export { default as Word } from "./Word";
-export { default as Answer } from "./Answer";
 export { default as UserForm } from "./UserForm";
 export { default as ScoreCounter } from "./ScoreCounter";
 export { default as LeaderBoardEntry } from "./LeaderBoardEntry";
