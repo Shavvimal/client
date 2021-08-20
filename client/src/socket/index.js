bc8aa4c743eabde88bbfc923c66870d24dc41fa7
Shav <shavhugan@gmail.com>
2021-08-20 11:18:13 +0100

    addd updated leaderboard

--- client/src/socket/index.js
+++ client/src/socket/index.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 
 
 
-const socket = io("/api2/*")
+const socket = io("http://localhost:5001/")
 
 socket.on("connect", () => {
     console.log(`Socket.io has connected to ${socket.id}!`)
